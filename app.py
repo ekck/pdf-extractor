c2c72adfd2763022edc8074033143a13b76f7130
kiprono <kiprono_emmanuel@yahoo.com>
2021-08-05 18:06:57 +0300

    pagination

--- app.py
+++ app.py
@@ -2,36 +2,56 @@ from tkinter import *
 import PyPDF2
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
-from functions import display_logo, display_textbox, extract_images, display_icon, resize_image, display_images 
+from functions import display_logo, display_textbox, display_icon, display_images, extract_images,  resize_image, copy_text, save_all, save_image
 
-page_contents = []
-
+#global parameters, updating dynamically
+all_content = []
 all_images = []
 img_idx = [0]
+displayed_img = []
 
-
-
-def copy_text(content):
-    root.clipboard_clear()
-    root.clipboard_append(content[-1])
-
-def save_all(images):
-    counter = 1 
-    for i in images :
-        if i.mode != "RGB":
-            i = i.convert("RGB")
-
-        i.save("img" + ".png", format="png")
-        counter += 1 
-
-def save_image(i):
-    if i.mode != "RGB":
-        i = i.convert("RGB")
-    i.save("img.png", format="png")
-
-
+#initiallize a Tkinter root object
 root = Tk()
 root.geometry('+%d+%d'%(350,10)) #place GUI at x=350, y=10
+
+#ARROW BUTTONS FUNCTIONALITY
+#right arrow
+def right_arrow(all_images, selected_img, what_text):
+    #restrict button actions to the number of avialable images
+    if img_idx[-1] < len(all_images) -1:
+        #change to the following index
+        new_idx = img_idx[-1] + 1
+        img_idx.pop()
+        img_idx.append(new_idx)
+        #remove displayed image if exists
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+        #create a new image in the new index & display it
+        new_img = all_images[img_idx[-1]]
+        selected_img = display_images(new_img)
+        displayed_img.append(selected_img)
+        #update the new index on the interface
+        what_text.set("image " + str(img_idx[-1] + 1) + " out of " + str(len(all_images)))
+
+#left arrow
+def left_arrow(all_images, selected_img, what_text):
+    #restrict button actions to indices greater than 1
+    if img_idx[-1] >= 1:
+        #change to the previous index
+        new_idx = img_idx[-1] - 1
+        img_idx.pop()
+        img_idx.append(new_idx)
+        #remove displayed image if exists
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+        #create a new image in the new index & display it
+        new_img = all_images[img_idx[-1]]
+        selected_img = display_images(new_img)
+        displayed_img.append(selected_img)
+        #update the new index on the interface
+        what_text.set("image " + str(img_idx[-1] + 1) + " out of " + str(len(all_images)))
 
 #header area - logo & browse button
 header = Frame(root, width=800, height=175, bg="white")
@@ -42,57 +62,90 @@ main_content = Frame(root, width=800, height=250, bg="#20bebe")
 main_content.grid(columnspan=3, rowspan=2, row=4)
 
 def open_file():
+
+    #clear global list of indices
+    for i in img_idx:
+        img_idx.pop()
+    img_idx.append(0) #set global index to 0
+
     browse_text.set("loading...")
+
+    #load a PDF file
     file = askopenfile(parent=root, mode='rb', filetypes=[("Pdf file", "*.pdf")])
     if file:
         read_pdf = PyPDF2.PdfFileReader(file)
+        #select a page
         page = read_pdf.getPage(0)
+        #extract text content from page
         page_content = page.extractText()
+
+        #SET A SPECIAL ENCODING OR REPLACE CHARACTERS
         #page_content = page_content.encode('cp1252')
         page_content = page_content.replace('\u2122', "'")
-        page_contents.append(page_content)
 
+        #CLEARING GLOBAL VARIABLES ONCE A NEW PDF FILE IS SELECTED
+        #clear the content of the previous PDF file
+        if all_content:
+            for i in all_content:
+                all_content.pop()
+
+        #clear the image list from the previous PDF file
+        for i in range(0, len(all_images)):
+            all_images.pop()
+
+        #hide the displayed image from the previous PDF file and remove it
+        if displayed_img:
+            displayed_img[-1].grid_forget()
+            displayed_img.pop()
+
+        #BEGIN EXTRACTING
+        #extract text
+        all_content.append(page_content)
+        #extract images
         images = extract_images(page)
+        for img in images:
+            all_images.append(img)
 
-        for i in images:
-            all_images.append(i)
+        #BEGIN DISPLAYING
+        #display the first image that was detected
+        selected_image = display_images(images[img_idx[-1]])
+        displayed_img.append(selected_image)
 
-        img = images[img_idx[-1]]
-
-      
-        display_images(img)
-
-
-
-        #show text box on row 2 col 0
-        display_textbox(page_content, 4, 0, root)
+        #display the text found on the page
+        display_textbox(all_content, 4, 0, root)
 
         #reset the button text back to Browse
         browse_text.set("Browse")
 
-
+        #BEGIN MENUES AND MENU WIDGETS
+        #1.image menu on row 2
         img_menu = Frame(root, width=800, height=60)
         img_menu.grid(columnspan=3, rowspan=1, row=2)
 
-        what_img = Label(root, text="image 1 of 5", font=("shanti", 10))
+        what_text = StringVar()
+        what_img = Label(root, textvariable=what_text, font=("shanti", 10))
+        what_text.set("image " + str(img_idx[-1] + 1) + " out of " + str(len(all_images)))
         what_img.grid(row=2, column=1)
 
-        display_icon('arrow_l.png', 2, 0, E)
-        display_icon('arrow_r.png', 2, 2, W)
+        #arrow buttons
+        display_icon('arrow_l.png', 2, 0, E, lambda:left_arrow(all_images, selected_image, what_text))
+        display_icon('arrow_r.png', 2, 2, W, lambda:right_arrow(all_images, selected_image, what_text))
 
-        save_img = Frame(root, width=800, height=60, bg="#c8c8c8")
-        header.grid(columnspan=3, rowspan=1, row=3)
+        #2.save image menu on row 3
+        save_img_menu = Frame(root, width=800, height=60, bg="#c8c8c8")
+        save_img_menu.grid(columnspan=3, rowspan=1, row=3)
 
-        copyText_btn = Button(root, text="copy text", command=lambda:copy_text(page_contents),font=("shanti", 10), height=1, width=15)
+        #create action buttons
+        copyText_btn = Button(root, text="copy text",command=lambda:copy_text(all_content, root), font=("shanti", 10), height=1, width=15)
         saveAll_btn = Button(root, text="save all images", command=lambda:save_all(all_images), font=("shanti", 10), height=1, width=15)
         save_btn = Button(root, text="save image", command=lambda:save_image(all_images[img_idx[-1]]), font=("shanti", 10), height=1, width=15)
 
-        copyText_btn.grid(row=3, column=0)
-        saveAll_btn.grid(row=3, column=1)
-        save_btn.grid(row=3, column=2)
+        #place buttons on grid
+        copyText_btn.grid(row=3,column=0)
+        saveAll_btn.grid(row=3,column=1)
+        save_btn.grid(row=3,column=2)
 
-
-
+#BEGIN INITIAL APP COMPONENTS
 display_logo('logo.png', 0, 0)
 
 #instructions
--- functions.py
+++ functions.py
@@ -4,19 +4,17 @@ from PIL import Image, ImageTk
 #place an image on the grid
 def display_logo(url, row, column):
     img = Image.open(url)
-    #resize image
     img = img.resize((int(img.size[0]/1.5),int(img.size[1]/1.5)))
     img = ImageTk.PhotoImage(img)
     img_label = Label(image=img, bg="white")
     img_label.image = img
     img_label.grid(column=column, row=row, rowspan=2, sticky=NW, padx=20, pady=40)
 
-def display_icon(url, row, column, stick):
+def display_icon(url, row, column, stick, funct):
     icon = Image.open(url)
-    #resize image
     icon = icon.resize((20,20))
     icon = ImageTk.PhotoImage(icon)
-    icon_label = Button(image=icon, width=25, height=25)
+    icon_label = Button(image=icon, command=funct, width=25, height=25)
     icon_label.image = icon
     icon_label.grid(column=column, row=row, sticky=stick)
 
@@ -28,11 +26,12 @@ def display_textbox(content, ro, col, root):
     text_box.tag_add("center", 1.0, "end")
     text_box.grid(column=col, row=ro, sticky=SW, padx=25, pady=25)
 
+#resizing the displayed image while keeping its ratio
 def resize_image(img):
     width, height = int(img.size[0]), int(img.size[1])
     if width > height:
         height = int(300/width*height)
-        width = 100
+        width = 300
     elif height > width:
         width = int(250/height*width)
         height = 250
@@ -41,6 +40,7 @@ def resize_image(img):
     img = img.resize((width, height))
     return img
 
+#display an image on the interface after resizing it
 def display_images(img):
     img = resize_image(img)
     img = ImageTk.PhotoImage(img)
@@ -48,9 +48,9 @@ def display_images(img):
     img_label.image = img
     img_label.grid(row=4, column=2, rowspan=2)
     return img_label
-        
 
 #Detect Images inside the PDF document
+#Thank you sylvain of Stackoverflow
 #https://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python
 def extract_images(page):
     images = []
@@ -69,3 +69,23 @@ def extract_images(page):
                 img = Image.frombytes(mode, size, data)
                 images.append(img)
     return images
+
+#SAVE IMAGE MENUE
+#button functionality
+
+def copy_text(content, root):
+    root.clipboard_clear()
+    root.clipboard_append(content[-1])
+
+def save_all(images):
+    counter = 1
+    for i in images:
+        if i.mode != "RGB":
+            i = i.convert("RGB")
+        i.save("img" + str(counter) + ".png", format="png")
+        counter += 1
+
+def save_image(img):
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+    img.save("img.png", format="png")
