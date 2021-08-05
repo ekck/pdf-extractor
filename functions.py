6a6bcb0c9caf548a1f130cdb0633bf32462359e4
kiprono <kiprono_emmanuel@yahoo.com>
2021-08-05 16:58:17 +0300

    save images btn

--- functions.py
+++ functions.py
@@ -11,6 +11,15 @@ def display_logo(url, row, column):
     img_label.image = img
     img_label.grid(column=column, row=row, rowspan=2, sticky=NW, padx=20, pady=40)
 
+def display_icon(url, row, column, stick):
+    icon = Image.open(url)
+    #resize image
+    icon = icon.resize((20,20))
+    icon = ImageTk.PhotoImage(icon)
+    icon_label = Button(image=icon, width=25, height=25)
+    icon_label.image = icon
+    icon_label.grid(column=column, row=row, sticky=stick)
+
 #place a tebox on the pages
 def display_textbox(content, ro, col, root):
     text_box = Text(root, height=10, width=30, padx=10, pady=10)
@@ -18,6 +27,28 @@ def display_textbox(content, ro, col, root):
     text_box.tag_configure("center", justify="center")
     text_box.tag_add("center", 1.0, "end")
     text_box.grid(column=col, row=ro, sticky=SW, padx=25, pady=25)
+
+def resize_image(img):
+    width, height = int(img.size[0]), int(img.size[1])
+    if width > height:
+        height = int(300/width*height)
+        width = 100
+    elif height > width:
+        width = int(250/height*width)
+        height = 250
+    else:
+        width, height = 250,250
+    img = img.resize((width, height))
+    return img
+
+def display_images(img):
+    img = resize_image(img)
+    img = ImageTk.PhotoImage(img)
+    img_label = Label(image=img, bg="white")
+    img_label.image = img
+    img_label.grid(row=4, column=2, rowspan=2)
+    return img_label
+        
 
 #Detect Images inside the PDF document
 #https://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python
